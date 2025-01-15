b5ec4970088ea330f46bcd8d8c9ef4d2ce1cc529
leejunyoung228 <80664840+leejunyoung228@users.noreply.github.com>
2025-01-15 17:48:55 +0900

    2025-01-15 17:48

--- src/main/java/com/green/studybridge/user/UserController.java
+++ src/main/java/com/green/studybridge/user/UserController.java
@@ -3,7 +3,10 @@ package com.green.studybridge.user;
 import com.green.studybridge.config.model.ResultResponse;
 import com.green.studybridge.user.entity.User;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
@@ -13,8 +16,8 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("sign-up")
-    public ResultResponse<Integer> signUp(@RequestBody User req) {
-        userService.signUp(req);
-        return null;
+    public ResultResponse<Integer> signUp(@RequestPart User req, @RequestPart("pic") MultipartFile mf) {
+        int result = userService.signUp(req, mf);
+        return ResultResponse.<Integer> builder().resultMessage("회원 가입 성공").resultData(result).build();
     }
 }
