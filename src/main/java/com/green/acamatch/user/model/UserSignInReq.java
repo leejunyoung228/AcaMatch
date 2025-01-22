588faf07cf8b541123e58dccb23fb6067cb07786
leejunyoung228 <80664840+leejunyoung228@users.noreply.github.com>
2025-01-22 10:36:29 +0900

    2025-01-22 10:35

--- src/main/java/com/green/acamatch/user/model/UserSignInReq.java
+++ src/main/java/com/green/acamatch/user/model/UserSignInReq.java
@@ -1,5 +1,6 @@
 package com.green.acamatch.user.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -10,9 +11,10 @@ public class UserSignInReq {
     @Email
     @NotNull
     private String email;
+    @Schema(description = "대소문자 특수문자 숫자 포함 8자 이상 20자 미만")
     @NotEmpty(message = "Password cannot be empty.")
     @Size(min = 8, max = 20, message = "Password must be between 8 and 20 characters.")
-    @Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#\\$%\\^&\\*])(?=\\S+$).{8,20}$",
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=\\S+$).{8,20}$",
             message = "Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character.")
     private String upw;
 }
