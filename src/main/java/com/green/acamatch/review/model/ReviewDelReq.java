22eb60c5386a7989808e8b4d9e170aded520a88a
Leejungmin <skdmldlgpdus0426@gmail.com>
2025-01-22 14:34:37 +0900

    Like & Review 응답 수정 및 스키마 수정

--- src/main/java/com/green/acamatch/review/model/ReviewDelReq.java
+++ src/main/java/com/green/acamatch/review/model/ReviewDelReq.java
@@ -9,12 +9,12 @@ import lombok.Setter;
 @Schema(description = "리뷰 삭제 요청")
 public class ReviewDelReq {
 
-    @Schema(title = "학원 ID", description = "학원 고유의 PK", example = "102")
+    @Schema(title = "학원 ID", description = "학원 고유의 PK", example = "324")
     private Long acaId;
 
-    @Schema(title = "수업 참여 ID", description = "삭제할 리뷰의 수업 참여 ID", example = "5")
+    @Schema(title = "수업 참여 ID", description = "삭제할 리뷰의 수업 참여 ID", example = "1")
     private long joinClassId;
 
-    @Schema(title = "유저 ID", description = "리뷰 삭제 요청을 한 유저의 ID", example = "2136")
+    @Schema(title = "유저 ID", description = "리뷰 삭제 요청을 한 유저의 ID", example = "1")
     private long userId;
 }
