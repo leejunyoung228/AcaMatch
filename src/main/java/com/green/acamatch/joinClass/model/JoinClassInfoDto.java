94db4346bb846bb498b4cc6bd5c11bac71388b8d
Gusuyoung <suyoung606123@gmail.com>
2025-02-03 17:52:37 +0900

    20250203(5)

--- src/main/java/com/green/acamatch/joinClass/model/JoinClassInfoDto.java
+++ src/main/java/com/green/acamatch/joinClass/model/JoinClassInfoDto.java
@@ -11,4 +11,8 @@ public class JoinClassInfoDto {
     private long classId;
     @Schema(title = "수업 이름", example = "초등 영어")
     private String className;
+    @Schema(title = "수업 시작 날짜", example = "2025-01-16")
+    private String startDate;
+    @Schema(title = "수업 종료 날짜", example = "2025-01-30")
+    private String endDate;
 }
