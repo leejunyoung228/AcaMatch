fbcacc80da91d581ebfef91c523e660aa22aa95c
HanByeo1Baek <starts466@gmail.com>
2025-02-04 14:17:51 +0900

    202502041417

--- src/main/java/com/green/acamatch/academy/model/GetAcademyDetailReq.java
+++ src/main/java/com/green/acamatch/academy/model/GetAcademyDetailReq.java
@@ -1,15 +1,20 @@
 package com.green.acamatch.academy.model;
 
+import com.green.acamatch.config.model.Paging;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
-public class GetAcademyDetailReq {
+public class GetAcademyDetailReq extends Paging{
     @Schema(title = "로그인한 유저 PK", example = "2119")
     private long signedUserId;
 
     @Schema(title = "학원 PK", example = "324")
     private long acaId;
+
+    public GetAcademyDetailReq(Integer page, Integer size, long signedUserId, long acaId, Integer startIdx, Integer size1) {
+        super(page, size);
+        this.signedUserId = signedUserId;
+        this.acaId = acaId;
+    }
 }
