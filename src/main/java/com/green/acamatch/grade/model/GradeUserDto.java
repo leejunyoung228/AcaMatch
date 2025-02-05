9eb7a1b6b90f0cb5f6c314c12176bbe5e29c637b
Gusuyoung <suyoung606123@gmail.com>
2025-02-05 16:22:23 +0900

    20250205

--- src/main/java/com/green/acamatch/grade/model/GradeUserDto.java
+++ src/main/java/com/green/acamatch/grade/model/GradeUserDto.java
@@ -10,11 +10,16 @@ import lombok.Setter;
 @JsonInclude(JsonInclude.Include.ALWAYS)
 public class GradeUserDto {
 
-    private long gradeId;
+    @Schema(title = "사용자 PK", example = "1", requiredMode = Schema.RequiredMode.REQUIRED)
+    private long userId;
     @Schema(title = "유저 사진")
     private String userPic;
     @Schema(title = "유저 이름", example = "홍길동")
     private String userName;
+    @Schema(title = "성적 PK", example = "1", requiredMode = Schema.RequiredMode.REQUIRED)
+    private long gradeId;
+    @Schema(title = "시험 이름", example = "1회 모의고사")
+    private String subjectName;
     @Schema(title = "시험 날짜", example = "2025-01-24")
     private String examDate;
     @Schema(title = "성적", example = "90")
