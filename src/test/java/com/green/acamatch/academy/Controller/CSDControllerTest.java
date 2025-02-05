2942b0a24732537ea6aebfa71fc76e58d06b7248
HanByeo1Baek <starts466@gmail.com>
2025-02-05 17:19:08 +0900

    제발

---
+++ src/test/java/com/green/acamatch/academy/Controller/CSDControllerTest.java
@@ -0,0 +1,20 @@
+package com.green.acamatch.academy.Controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CSDControllerTest {
+
+    @Test
+    void getCityList() {
+    }
+
+    @Test
+    void getStreetList() {
+    }
+
+    @Test
+    void getDongList() {
+    }
+}
