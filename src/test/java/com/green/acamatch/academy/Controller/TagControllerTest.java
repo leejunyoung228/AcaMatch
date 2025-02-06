e917a8775928053a7627f6cc27cfee55fa6115a3
jangjaeung <wodnd1998@gmail.com>
2025-02-06 13:03:21 +0900

    academy(TDD 작성중)

--- src/test/java/com/green/acamatch/academy/Controller/TagControllerTest.java
+++ src/test/java/com/green/acamatch/academy/Controller/TagControllerTest.java
@@ -2,17 +2,25 @@ package com.green.acamatch.academy.Controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.green.acamatch.academy.Service.TagService;
+import com.green.acamatch.academy.model.JW.AcademyMessage;
 import com.green.acamatch.academy.tag.SelTagDto;
 import com.green.acamatch.academy.tag.SelTagReq;
 import com.green.acamatch.academy.tag.SelTagRes;
+import com.green.acamatch.config.MyFileUtils;
+import com.green.acamatch.config.constant.JwtConst;
+import com.green.acamatch.config.jwt.JwtTokenProvider;
 import com.green.acamatch.config.model.ResultResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.http.MediaType;
+
+import org.springframework.context.annotation.Import;
+
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.util.LinkedMultiValueMap;
@@ -21,46 +29,64 @@ import org.springframework.util.MultiValueMap;
 import java.util.List;
 
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
+@Import(JwtTokenProvider.class)
 @WebMvcTest(
         controllers = TagController.class
         , excludeAutoConfiguration = SecurityAutoConfiguration.class
 )
+@EnableConfigurationProperties(JwtConst.class)
 class TagControllerTest {
 
     @Autowired
     ObjectMapper objectMapper;
     @Autowired
     MockMvc mockMvc;
-    @Mock
+    @MockitoBean
+    private AcademyMessage academyMessage;
+
+    @MockitoBean
     TagService tagService;
 
-    final String BASE_URL = "/api/academy/tag";
+    final long tagId_9 = 9L;
+    final String tagName_9 = "고등수학";
+    final String BASE_URL = "/api/tag";
 
     @Test
     @DisplayName("태그 불러오기")
     void selTagList() throws Exception{
         SelTagReq givenParam = new SelTagReq();
-        givenParam.setSearchTag("국어");
+        givenParam.setSearchTag(tagName_9);
 
+        SelTagDto selTagDto = new SelTagDto();
+        selTagDto.setTagId(tagId_9);
+        selTagDto.setTagName(tagName_9);
 
         SelTagRes expectedResult = new SelTagRes();
-        expectedResult.setSelTagList(List.of(new SelTagDto()));
+        expectedResult.setSelTagList(List.of(selTagDto));
 
         given(tagService.selTagList(givenParam)).willReturn(expectedResult);
-        ResultActions resultActions;
+        ResultActions resultActions = mockMvc.perform(get(BASE_URL).queryParams(getParameter(givenParam)));
+        String expectedResJson;
+        expectedResJson = getExpectedResJson(expectedResult);
 
-        resultActions = mockMvc.perform(get(BASE_URL).queryParams(getParameter(givenParm)));
 
-    String expectedResJson;
-    expectedResJson = getExpectedResJson(expectedResult);
+    resultActions.andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(content().json(expectedResJson));
+
+    verify(tagService).selTagList(givenParam);
 
     }
 
     private MultiValueMap<String, String> getParameter(SelTagReq givenParam) {
-        MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<>(3);
+        MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<>(1);
 
         queryParams.add("searchTag", String.valueOf(givenParam.getSearchTag()));
         return queryParams;
