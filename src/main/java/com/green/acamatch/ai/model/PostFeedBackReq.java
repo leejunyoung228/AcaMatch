b8cd280608d7d5a935c439763b3a3a7532334fa0
HanByeo1Baek <starts466@gmail.com>
2025-02-07 11:52:08 +0900

    AI 피드백 추가

--- src/main/java/com/green/acamatch/ai/model/PostFeedBackReq.java
+++ src/main/java/com/green/acamatch/ai/model/PostFeedBackReq.java
@@ -1,4 +1,19 @@
 package com.green.acamatch.ai.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class PostFeedBackReq {
+    @Schema(title = "grade PK", example = "1")
+    private Integer gradeId;
+
+    @Schema(title = "AI 피드백", example = "피드백")
+    private String feedBack;
+
+    @JsonIgnore
+    private String message;
 }
