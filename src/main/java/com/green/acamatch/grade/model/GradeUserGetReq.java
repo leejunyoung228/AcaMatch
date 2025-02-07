6ebbc32781b87343cfeb5dc26d1ce7be0fcb0fa4
Gusuyoung <suyoung606123@gmail.com>
2025-02-07 19:42:35 +0900

    20250207

--- src/main/java/com/green/acamatch/grade/model/GradeUserGetReq.java
+++ src/main/java/com/green/acamatch/grade/model/GradeUserGetReq.java
@@ -8,17 +8,17 @@ import lombok.Setter;
 @Setter
 @Getter
 public class GradeUserGetReq extends Paging {
-    @Schema(title = "학원 PK", example = "1", requiredMode = Schema.RequiredMode.REQUIRED)
-    private long acaId;
-    @Schema(title = "joinClass PK", example = "1", requiredMode = Schema.RequiredMode.REQUIRED)
-    private long joinClassId;
+//    @Schema(title = "학원 PK", example = "1", requiredMode = Schema.RequiredMode.REQUIRED)
+//    private long acaId;
+//    @Schema(title = "joinClass PK", example = "1", requiredMode = Schema.RequiredMode.REQUIRED)
+//    private long joinClassId;
     @Schema(title = "시험 회차 PK", example = "1", requiredMode = Schema.RequiredMode.REQUIRED)
     private long subjectId;
 
-    public GradeUserGetReq(Integer page, Integer size, long acaId, long joinClassId, long subjectId) {
+    public GradeUserGetReq(Integer page, Integer size, long subjectId) {
         super(page, size);
-        this.acaId = acaId;
-        this.joinClassId = joinClassId;
+//        this.acaId = acaId;
+//        this.joinClassId = joinClassId;
         this.subjectId = subjectId;
     }
 }
