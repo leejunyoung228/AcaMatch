8db2636213f75055002aed8cc08d63052c8f03b3
Gusuyoung <suyoung606123@gmail.com>
2025-02-10 10:38:26 +0900

    20250210

--- src/main/java/com/green/acamatch/grade/model/GradePutReq.java
+++ src/main/java/com/green/acamatch/grade/model/GradePutReq.java
@@ -13,6 +13,8 @@ public class GradePutReq {
 
     @Schema(title = "성적 PK", example = "1", requiredMode = Schema.RequiredMode.REQUIRED)
     private long gradeId;
+    @Schema(title = "시험 날짜", example = "2025-02-10")
+    private String examDate;
     @Schema(title = "성적", example = "10")
     private Integer score;
     @Schema(title = "통과 여부", example = "1")
