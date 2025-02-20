35849f55b2a3d55cf5b74b98b544044555b2e482
leejunyoung <junyeongi748@gmail.com>
2025-02-20 12:36:25 +0900

    2025-02-20 12:36

--- src/main/java/com/green/acamatch/entity/user/SnsUserId.java
+++ src/main/java/com/green/acamatch/entity/user/SnsUserId.java
@@ -1,6 +1,7 @@
 package com.green.acamatch.entity.user;
 
 import com.green.acamatch.entity.myenum.SignInProviderType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.*;
 
@@ -11,6 +12,8 @@ import lombok.*;
 @NoArgsConstructor  // 기본 생성자 추가
 @AllArgsConstructor
 public class SnsUserId {
+    @Column(name = "user_id")
     private Long userId;
+    @Column
     private SignInProviderType signInProviderType;
 }
