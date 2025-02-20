7bb66ce2a1458e02ca2665e396a081d6a45b8502
Leejungmin0426 <skdmldlgpdus0426@gmail.com>
2025-02-20 18:22:53 +0900

    Revert "2025-02-20 15:33 user valid 추가"

--- src/main/java/com/green/acamatch/user/model/SimpleUserDataUpdateReq.java
+++ src/main/java/com/green/acamatch/user/model/SimpleUserDataUpdateReq.java
@@ -1,7 +1,6 @@
 package com.green.acamatch.user.model;
 
 import com.green.acamatch.entity.myenum.UserRole;
-import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,18 +9,11 @@ import java.time.LocalDate;
 @Getter
 @Setter
 public class SimpleUserDataUpdateReq {
-    @NotEmpty
     private Long userId;
-    @NotEmpty
     private UserRole userRole;
-    @NotEmpty
     private String name;
-    @NotEmpty
     private String phone;
-    @NotEmpty
     private LocalDate birth;
-    @NotEmpty
     private String nickName;
-    @NotEmpty
     private String upw;
 }
--- src/main/java/com/green/acamatch/user/model/UserSignUpReq.java
+++ src/main/java/com/green/acamatch/user/model/UserSignUpReq.java
@@ -13,20 +13,15 @@ import java.time.LocalDate;
 @EqualsAndHashCode
 @AllArgsConstructor
 public class UserSignUpReq {
-    @NotEmpty
     private UserRole userRole;
-    @NotEmpty
     private String name;
     @Pattern(regexp = "^(01[0-9])-\\d{3,4}-\\d{4}$", message = "Invalid phone number format. Example: 010-1234-5678")
     private String phone;
-    @NotEmpty
     private LocalDate birth;
-    @NotEmpty
     private String nickName;
     @Email
-    @NotEmpty
+    @NotNull
     private String email;
-    @NotEmpty
     @Schema(description = "대소문자 특수문자 숫자 포함 8자 이상 20자 미만")
     @Size(min = 8, max = 20, message = "Password must be between 8 and 20 characters.")
     @Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*()_+\\[\\]{}|;:,.<>?])(?=\\S+$).{8,20}$",
