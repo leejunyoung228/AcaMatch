470bee3ca86d81d533fe5117e1459ffe44d1bf0b
Leejungmin <skdmldlgpdus0426@gmail.com>
2025-02-24 12:18:55 +0900

    like teacher entity 수정

--- src/main/java/com/green/acamatch/entity/manager/TeacherIds.java
+++ src/main/java/com/green/acamatch/entity/manager/TeacherIds.java
@@ -5,11 +5,13 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @Getter
 @Setter
 @Embeddable
 @EqualsAndHashCode
-public class TeacherIds {
+public class TeacherIds implements Serializable {
     private Long userId;
     private Long acaId;
 }
