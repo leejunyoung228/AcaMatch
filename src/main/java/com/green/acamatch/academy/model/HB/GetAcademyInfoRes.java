2ee882036ee11ef60e8c82f89f0f8f0b6ef0f3ca
HanByeo1Baek <starts466@gmail.com>
2025-02-27 13:00:57 +0900

    검색 기능 추가

---
+++ src/main/java/com/green/acamatch/academy/model/HB/GetAcademyInfoRes.java
@@ -0,0 +1,17 @@
+package com.green.acamatch.academy.model.HB;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class GetAcademyInfoRes {
+    private String acaName;
+    private String acaPic;
+    private String acaPics;
+    private String comment;
+    private String createdAt;
+    private String acaPhone;
+    private String address;
+    private long userId;
+}
