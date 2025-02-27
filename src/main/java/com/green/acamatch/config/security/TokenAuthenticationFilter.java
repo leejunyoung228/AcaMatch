defc95ffeef47040ac336e20dcd3c7a5780b8f85
Leejungmin <skdmldlgpdus0426@gmail.com>
2025-02-27 09:37:30 +0900

    미친듯이 수정 중 >> 매니저 서비스 , sms, 클래스 레파지토리, 선생님 서비스, 토큰우덴티케이샨필터 등등.... 이상하면 롤백합니다.

--- src/main/java/com/green/acamatch/config/security/TokenAuthenticationFilter.java
+++ src/main/java/com/green/acamatch/config/security/TokenAuthenticationFilter.java
@@ -2,6 +2,7 @@ package com.green.acamatch.config.security;
 
 import com.green.acamatch.config.exception.UserErrorCode;
 import com.green.acamatch.config.jwt.JwtTokenProvider;
+import com.green.acamatch.config.jwt.JwtUser;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.MalformedJwtException;
 import jakarta.servlet.FilterChain;
@@ -10,6 +11,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -17,18 +19,22 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-@Slf4j
 @Component
 @RequiredArgsConstructor
 public class TokenAuthenticationFilter extends OncePerRequestFilter {
     private final JwtTokenProvider jwtTokenProvider;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
         String token = jwtTokenProvider.resolveToken(request);
+
         if (token != null) {
             try {
-                Authentication authentication = jwtTokenProvider.getAuthentication(token);
+                JwtUser jwtUser = jwtTokenProvider.getJwtUserFromToken(token); // JWT에서 사용자 정보 가져오기
+                UsernamePasswordAuthenticationToken authentication =
+                        new UsernamePasswordAuthenticationToken(jwtUser, null, jwtUser.getAuthorities());
+
                 SecurityContextHolder.getContext().setAuthentication(authentication);
             } catch (ExpiredJwtException e) {
                 response.sendError(HttpServletResponse.SC_UNAUTHORIZED, UserErrorCode.EXPIRED_TOKEN.getMessage());
