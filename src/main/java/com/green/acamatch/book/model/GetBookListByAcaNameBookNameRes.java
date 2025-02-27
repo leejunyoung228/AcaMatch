14350e8a32ee8f9dd518e531b86aebb5c0c2bae4
HanByeo1Baek <starts466@gmail.com>
2025-02-27 14:57:55 +0900

    관리자가 책 검색 기능 추가

---
+++ src/main/java/com/green/acamatch/book/model/GetBookListByAcaNameBookNameRes.java
@@ -0,0 +1,18 @@
+package com.green.acamatch.book.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class GetBookListByAcaNameBookNameRes {
+    private long bookId;
+    private String bookName;
+    private String bookComment;
+    private String bookPic;
+    private int bookAmount;
+    private int bookPrice;
+    private String manager;
+    private String createdAt;
+    private String bookCount;
+}
