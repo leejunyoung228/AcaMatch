7f7125a9793aaa1598583ad4aae071bdb9e84177
HanByeo1Baek <starts466@gmail.com>
2025-03-07 16:32:03 +0900

    리뷰 정지 조치를 당한 뒤 정지된 기간동안 해당 리뷰만 보이지 않도록 수정, 수업 결제시에 이미 수강중이면 오류 터지도록 수정

---
+++ src/main/java/com/green/acamatch/academy/model/HB/ReviewPicDto.java
@@ -0,0 +1,11 @@
+package com.green.acamatch.academy.model.HB;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ReviewPicDto {
+    private long reviewId;
+    private String reviewPic;
+}
