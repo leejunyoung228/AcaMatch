99766ef4dbec9a421f0aa66d064322d39ee176ec
HanByeo1Baek <starts466@gmail.com>
2025-03-10 15:59:39 +0900

    대시보드 학원, 결제, 유저 수 추가

--- src/main/java/com/green/acamatch/reports/ReportsMapper.java
+++ src/main/java/com/green/acamatch/reports/ReportsMapper.java
@@ -1,7 +1,11 @@
 package com.green.acamatch.reports;
 
+import com.green.acamatch.reports.model.GetUserListRes;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface ReportsMapper {
+    List<GetUserListRes> getUserList();
 }
