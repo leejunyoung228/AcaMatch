49a8771c5b1652ae70aab04bd23d68e51c90fb6c
HanByeo1Baek <starts466@gmail.com>
2025-03-13 16:36:13 +0900

    프론트 빌드 파일 적용

--- src/main/java/com/green/acamatch/AcaMatchApplication.java
+++ src/main/java/com/green/acamatch/AcaMatchApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
