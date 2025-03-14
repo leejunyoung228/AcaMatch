b9ab88d5b0c96bb4301d626164592981bb7a3550
Gusuyoung <suyoung606123@gmail.com>
2025-03-14 18:46:38 +0900

    20250314

---
+++ src/main/java/com/green/acamatch/menuOut/model/MenuOutAcademyAllGetRes.java
@@ -0,0 +1,13 @@
+package com.green.acamatch.menuOut.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class MenuOutAcademyAllGetRes {
+    private Long acaId;
+    private String acaName;
+}
