10b55bb1ef0fcb230e9cd17d14566e6f9ed18773
HanByeo1Baek <starts466@gmail.com>
2025-03-14 18:47:16 +0900

    리뷰 다 지우고 만들었음

---
+++ src/main/java/com/green/acamatch/review/ReviewRepository.java
@@ -0,0 +1,9 @@
+package com.green.acamatch.review;
+
+import com.green.acamatch.entity.review.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+}
